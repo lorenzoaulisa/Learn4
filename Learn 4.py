63e6a8fdffeade2ec8118bc7a1dda4238da79704
lorenzoaulisa <lorenzoaulisa05@gmail.com>
2019-10-14 08:44:05 -0500

    archimedes

---
+++ Learn 4.py
@@ -0,0 +1,20 @@
+# Starting off
+print(22/7)
+print(355/113)
+import math
+
+print(9801/(2206 * math.sqrt(2)))
+
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumference = numSides * sideS
+    pi = polygonCircumference / 2
+    return pi
+
+
+print(archimedes(8))
+print(archimedes(16))
