94907d2b77d5d0fa9700c79dc4f7cb820b908171
lorenzoaulisa <lorenzoaulisa05@gmail.com>
2019-10-17 09:01:23 -0500

    Accumalators and tasks

--- Learn 4.py
+++ Learn 4.py
@@ -25,23 +25,48 @@ for sides in range(8, 999999, 8):
 # A: I'm guessing infinitely many
 
 # Accumulators
+
+# Commute the sum of the first 100 even numbers
 acc = 0
 for x in range(1, 101):
     acc = acc + x * 2
 
 print(acc)
 
-acc = 0
-for x in range(1, 51):
+# Commute the sum of the first 50 odd numbers
+acc = 50
+for x in range(1, 50):
     acc = acc + x * 2
 
 print(acc)
 
-# Commute the sum of the first 100 even numbers
-# Commute the sum of the first 50 odd numbers
 # Commute the average of the first 100 odd numbers
+acc = 0
+for x in range(1, 200):
+    if x % 2 :
+        acc = (acc + x)
+
+print(acc/100)
+# Commute the sum of the first 100 even numbers
+# Complete
+
+# Commute the sum of the first 50 odd numbers
+# Complete
+
+# Commute the average of the first 100 odd numbers
+# Complete
+
 # write a function that returns the average of the first N numbers, where N is a parameter
+
+
 # Write a function called factorial that commutes the product of the first N numbers, where N is a parameter
+
+
 # Each number in the Fibonacci sequence is the sum of the previous 2 numbers
+
+
 # The first two numbers in the sequence are 1 and 1. Commute the 10th Fibonacci number
+
+
 # You may assume that N will be greater or equal to 3.
+
